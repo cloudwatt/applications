b8acc94cc654c75b2faf3e6be44ace99319c2c07
sploadie <tanguygauvrit@gmail.com>
2015-09-03 17:21:03 +0200

    LDAP stack-start.sh should not continue upon error

--- bundle-trusty-ldap/stack-start.sh
+++ bundle-trusty-ldap/stack-start.sh
@@ -2,13 +2,14 @@
 
 if [ ! "$2" ]; then
   echo "\
-Usage: ./stack-start.sh STACK_NAME KEYPAIR_NAME [BACKUP_ID]
-
-Include backup ID only when restoring from backup."
+Usage: ./stack-start.sh STACK_NAME SUBNET_ID KEYPAIR_NAME [BACKUP_ID]
+Include backup ID only when restoring from backup.
+"
+  exit 1
 fi
 
-if [ ! "$3" ]; then
-  heat stack-create $1 -f bundle-trusty-ldap.heat.yml -P keypair_name="$2"
+if [ ! "$4" ]; then
+  heat stack-create $1 -f bundle-trusty-ldap.heat.yml -P subnet_id="$2" -P keypair_name="$3"
 else
-  heat stack-create $1 -f bundle-trusty-ldap.restore.heat.yml -P keypair_name="$2" -P backup_id="$3"
+  heat stack-create $1 -f bundle-trusty-ldap.restore.heat.yml -P subnet_id="$2" -P keypair_name="$3" -P backup_id="$4"
 fi
