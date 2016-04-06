ffa76a7fce56b90434f0288b5613807d5b039d18
Xcite <julien.deplaix@cloudwatt.com>
2016-04-06 10:42:54 +0200

    remove clamav

--- bundle-trusty-clamav/stack-get-url.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo $1 $(heat resource-list $1 | grep floating_ip_link | tr -d " " | cut -d "|" -f3 | rev | cut -d "-" -f1 | rev)
--- bundle-trusty-clamav/stack-start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-heat stack-create $1 -f bundle-trusty-clamav.heat.yml
